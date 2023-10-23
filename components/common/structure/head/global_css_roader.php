b7bea6f3bc95fe92d85ada31a928aa1f3bb05eb5
Shunya <shunya.kawaguchi@raquty.com>
2023-10-23 23:07:04 +0900

    ✅operation用のcss上書き作成

---
+++ components/common/structure/head/global_css_roader.php
@@ -0,0 +1,9 @@
+<?php
+//Operation関係のページならglobal-cssを上書きするcssを読み込む
+//必ずglobal-csが読み込まれた後に関数を発動する
+
+function overwrite_global_css($template) {
+    if($template == 'operation' || $template == 'operation_oop'|| $template == 'operation_tournament'|| $template == 'operation_court_situation'){
+        echo '<link rel="stylesheet" href="/components/common/overwrite_global-css.min.css">';
+    }
+}
--- components/common/structure/head/head.php
+++ components/common/structure/head/head.php
@@ -1,11 +1,14 @@
 <?php 
-    if($template !== 'login'){
-      $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http";
-      $host = $_SERVER['HTTP_HOST'];
-      $uri = $_SERVER['REQUEST_URI'];
-      $full_url = $protocol . "://" . $host . $uri;
-      $_SESSION['request_url'] = $full_url;
-    }
+//タイトル表示関数呼び出し
+require_once(dirname(__FILE__).'/global_css_roader.php');
+
+if($template !== 'login'){
+  $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http";
+  $host = $_SERVER['HTTP_HOST'];
+  $uri = $_SERVER['REQUEST_URI'];
+  $full_url = $protocol . "://" . $host . $uri;
+  $_SESSION['request_url'] = $full_url;
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -17,6 +20,7 @@
     <meta name="robots" content="noindex">
     <link rel="icon" href="/files/material/raquty_fabicon.ico" id="favicon">
     <link rel="stylesheet" href="/components/common/global-css.min.css">
+    <?php overwrite_global_css($template); ?>
     <link rel="stylesheet" href="/components/templates/<?php echo  $template.'/' .$template.'.min.css' ;?>">
     <?php require_once(dirname(__FILE__).'/../structure-css_loder.php') ; ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
